97217c739bc9859729f699dbffd253b2c7118eb6
Evelyn Cleto <67518341+EvelynCleto@users.noreply.github.com>
2024-05-16 20:59:48 -0300

    Update apagar.js

--- futstats/js/apagar.js
+++ futstats/js/apagar.js
@@ -1,11 +1,14 @@
 function apagar(id){
+    // Convertendo o ID passado para o mesmo tipo do ID do jogador
+    id = parseInt(id);
+
     // Pegar os jogadores do localStorage
     let jogadores = JSON.parse(localStorage.getItem("jogadores")) || [];
 
     // Filtrar os jogadores para remover o jogador com o id passado como parâmetro
     jogadores = jogadores.filter(jogador => jogador.id !== id);
 
-    // Salvar a lista de jogadores no localStorage
+    // Salvar a lista de jogadores atualizada no localStorage
     localStorage.setItem("jogadores", JSON.stringify(jogadores));
 
     // Remover o item da página ou recarregar a página
